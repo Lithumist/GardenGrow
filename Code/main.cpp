f02b26ddc74c366641ec4374a6196941e7f03ce4
Pedro Custodio <magneticplum@live.co.uk>
2014-05-13 12:01:11 +0100

    first commit

---
+++ Code/main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+
+#include "options.h"
+
+int main()
+{
+    sf::RenderWindow gameWindow( sf::VideoMode (GG_WINDOW_WIDTH,GG_WINDOW_HEIGHT) , GG_WINDOW_CAPTION , sf::Style::Titlebar | sf::Style::Close );
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (gameWindow.isOpen())
+    {
+        sf::Event event;
+        while (gameWindow.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                gameWindow.close();
+        }
+
+        gameWindow.clear();
+        gameWindow.draw(shape);
+        gameWindow.display();
+    }
+
+    return 0;
+}
---
+++ Code/options.h
@@ -0,0 +1,14 @@
+// Part of GardenGrow
+
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+/*
+    Declares program option macros
+*/
+
+#define GG_WINDOW_WIDTH 800
+#define GG_WINDOW_HEIGHT 600
+#define GG_WINDOW_CAPTION "Garden Grow"
+
+#endif
